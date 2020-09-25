47704f1823cbcc89dc199a489c72663968e1bf65
Konstantinov <misha.tank.2014@mail.ru>
2020-09-26 00:35:23 +0400

    удалил комментарии и расставил пробелы/скобки

--- src/main/java/ru/ssau/tk/GussiRep/labOOP/functions/MathFunction.java
+++ src/main/java/ru/ssau/tk/GussiRep/labOOP/functions/MathFunction.java
@@ -1,5 +1,6 @@
 package ru.ssau.tk.GussiRep.labOOP.functions;
 
 public interface MathFunction {
+
     double apply(double x);
 }
--- src/main/java/ru/ssau/tk/GussiRep/labOOP/functions/PowFunction.java
+++ src/main/java/ru/ssau/tk/GussiRep/labOOP/functions/PowFunction.java
@@ -1,11 +1,6 @@
 package ru.ssau.tk.GussiRep.labOOP.functions;
 
 public class PowFunction implements MathFunction {
-/*
-    public final double y;
-    public PowFunction(double y){
-        this.y=y;
-    } */
     @Override
     public double apply(double x) {
         return Math.pow(x, x);
