d5acc5fdc1488407850f6666b01e5f597b2761d1
BeatsBoyXZP <bgv-gleb01@yandex.ru>
2020-09-26 08:47:53 +0300

    Задали функцию для нахождения арксинуса: "asinFunctionTest"

---
+++ src/main/java/ru/ssau/tk/GussiRep/labOOP/functions/asinFunction.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.GussiRep.labOOP.functions;
+
+public class asinFunction implements MathFunction {
+
+    @Override
+    public double apply(double x) {
+        return Math.asin(x);
+    }
+}
