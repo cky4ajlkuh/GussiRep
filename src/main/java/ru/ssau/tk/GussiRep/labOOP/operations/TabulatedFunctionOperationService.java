d34f1bfe6eaec92f088fd67d3aa987a4abc0e177
BeatsBoyXZP <bgv-gleb01@yandex.ru>
2020-11-03 18:12:32 +0300

    Не доделал Service.

--- src/main/java/ru/ssau/tk/GussiRep/labOOP/operations/TabulatedFunctionOperationService.java
+++ src/main/java/ru/ssau/tk/GussiRep/labOOP/operations/TabulatedFunctionOperationService.java
@@ -1,7 +1,11 @@
 package ru.ssau.tk.GussiRep.labOOP.operations;
 
+import ru.ssau.tk.GussiRep.labOOP.functions.Point;
+import ru.ssau.tk.GussiRep.labOOP.functions.TabulatedFunction;
+
 public class TabulatedFunctionOperationService {
-    public static void Point[] asPoints(TabulatedFunction tabulatedFunction){
+    public static Point[] asPoints(TabulatedFunction tabulatedFunction){
+        int i=0;
 
     }
 }
