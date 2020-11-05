1849ae160bd9e0cf7f3e709dadde64981d687e0e
BeatsBoyXZP <bgv-gleb01@yandex.ru>
2020-11-05 21:59:38 +0300

    Доделал Service.

--- src/main/java/ru/ssau/tk/GussiRep/labOOP/operations/TabulatedFunctionOperationService.java
+++ src/main/java/ru/ssau/tk/GussiRep/labOOP/operations/TabulatedFunctionOperationService.java
@@ -4,7 +4,12 @@ import ru.ssau.tk.GussiRep.labOOP.functions.Point;
 import ru.ssau.tk.GussiRep.labOOP.functions.TabulatedFunction;
 
 public class TabulatedFunctionOperationService {
-    //public static Point[] asPoints(TabulatedFunction tabulatedFunction){
-     //   int i=0;
-   // }
+    public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
+        int i = 0;
+        Point[] points = new Point[tabulatedFunction.getCount()];
+        for (Point newPoint : tabulatedFunction) {
+            points[i++] = newPoint;
+        }
+        return points;
+    }
 }
