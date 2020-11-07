9cb35e44da22d2d1e618cfecfc0fd8ffc81881d7
Konstantinov <misha.tank.2014@mail.ru>
2020-11-07 14:46:06 +0400

    testing TabulatedFunctionOperationService

--- src/test/java/ru/ssau/tk/GussiRep/labOOP/operations/TabulatedFunctionOperationServiceTest.java
+++ src/test/java/ru/ssau/tk/GussiRep/labOOP/operations/TabulatedFunctionOperationServiceTest.java
@@ -1,6 +1,7 @@
 package ru.ssau.tk.GussiRep.labOOP.operations;
 
 import org.testng.annotations.Test;
+import ru.ssau.tk.GussiRep.labOOP.exceptions.InconsistentFunctionsException;
 import ru.ssau.tk.GussiRep.labOOP.functions.ArrayTabulatedFunction;
 import ru.ssau.tk.GussiRep.labOOP.functions.LinkedListTabulatedFunction;
 import ru.ssau.tk.GussiRep.labOOP.functions.Point;
@@ -15,7 +16,6 @@ public class TabulatedFunctionOperationServiceTest {
     private final static double[] xValues = new double[]{1., 4., 9., 16., 25., 36.};
     private final static double[] yValues = new double[]{5., 12., 89., 5., 56., 1.};
     private final static double[] yValuesSecond = new double[]{1., 4., 7., 16., 15., 3.};
-
 
     TabulatedFunction functionFirst = new ArrayTabulatedFunction(xValues, yValues);
     LinkedListTabulatedFunction functionSecond = new LinkedListTabulatedFunction(xValues, yValuesSecond);
@@ -48,7 +48,7 @@ public class TabulatedFunctionOperationServiceTest {
             assertEquals(pointsA[i].y, functionFirst.getY(i), 0.001);
         }
 
-        for (int i = 0 ; i < xValues.length ; i++){
+        for (int i = 0; i < xValues.length; i++) {
             assertEquals(pointsB[i].x, functionSecond.getX(i), 0.001);
             assertEquals(pointsB[i].y, functionSecond.getY(i), 0.001);
         }
@@ -56,18 +56,99 @@ public class TabulatedFunctionOperationServiceTest {
 
     @Test
     public void testSum() {
-
+        LinkedListTabulatedFunctionFactory factoryLLF = new LinkedListTabulatedFunctionFactory();
+        TabulatedFunctionOperationService service1 = new TabulatedFunctionOperationService();
+        TabulatedFunctionOperationService service2 = new TabulatedFunctionOperationService(factoryLLF);
+        TabulatedFunction a = new ArrayTabulatedFunctionFactory().create(xValues, yValues);
+        TabulatedFunction b = factoryLLF.create(xValues, yValuesSecond);
+        TabulatedFunction sumATF = service1.sum(a, b);
+        TabulatedFunction sumLLF = service2.sum(a, b);
+        int i = 0;
+        for (Point point : sumATF) {
+            assertEquals(point.x, xValues[i]);
+            assertEquals(point.y, yValues[i] + yValuesSecond[i++]);
+        }
+        int j = 0;
+        for (Point point : sumLLF) {
+            assertEquals(point.x, xValues[j]);
+            assertEquals(point.y, yValues[j] + yValuesSecond[j++]);
+        }
+        assertThrows(InconsistentFunctionsException.class, () ->
+                service1.sum(factoryLLF.create(new double[]{1., 2., 3}, new double[]{2., 6., 5.}), b));
     }
 
     @Test
     public void testSubtract() {
+        LinkedListTabulatedFunctionFactory factoryLLF = new LinkedListTabulatedFunctionFactory();
+        TabulatedFunctionOperationService service1 = new TabulatedFunctionOperationService();
+        TabulatedFunctionOperationService service2 = new TabulatedFunctionOperationService(factoryLLF);
+        ArrayTabulatedFunctionFactory factoryATF = new ArrayTabulatedFunctionFactory();
+        TabulatedFunction a = new ArrayTabulatedFunctionFactory().create(xValues, yValues);
+        TabulatedFunction b = factoryLLF.create(xValues, yValuesSecond);
+        TabulatedFunction subtractATF = service1.subtract(a, b);
+        TabulatedFunction subtractLLF = service2.subtract(a, b);
+        int i = 0;
+        for (Point point : subtractATF) {
+            assertEquals(point.x, xValues[i]);
+            assertEquals(point.y, yValues[i] - yValuesSecond[i++]);
+        }
+        int j = 0;
+        for (Point point : subtractLLF) {
+            assertEquals(point.x, xValues[j]);
+            assertEquals(point.y, yValues[j] - yValuesSecond[j++]);
+        }
+        assertThrows(InconsistentFunctionsException.class, () ->
+                service2.subtract(factoryATF.create(new double[]{-4., 0., 3., 18.}, new double[]{-5., -2., 5., 6.}), b));
     }
 
     @Test
     public void testMultiply() {
+        double[] arrayX = new double[]{-10, -5, 0, 5, 10, 15};
+        double[] arrayY = new double[]{0.5, 1.5, 2.5, 3.5, 4.5, 5.5};
+        double[] secondArrayY = new double[]{0., 2., 4., 6., 8., 10.};
+        LinkedListTabulatedFunctionFactory factoryLLF = new LinkedListTabulatedFunctionFactory();
+        ArrayTabulatedFunctionFactory factoryATF = new ArrayTabulatedFunctionFactory();
+        TabulatedFunctionOperationService service1 = new TabulatedFunctionOperationService();
+        TabulatedFunctionOperationService service2 = new TabulatedFunctionOperationService(factoryLLF);
+        TabulatedFunction a = new ArrayTabulatedFunctionFactory().create(arrayX, arrayY);
+        TabulatedFunction b = factoryLLF.create(arrayX, secondArrayY);
+        TabulatedFunction subtractATF = service1.multiply(a, b);
+        TabulatedFunction subtractLLF = service2.multiply(a, b);
+        int i = 0;
+        for (Point point : subtractATF) {
+            assertEquals(point.x, arrayX[i]);
+            assertEquals(point.y, arrayY[i] * secondArrayY[i++]);
+        }
+        int j = 0;
+        for (Point point : subtractLLF) {
+            assertEquals(point.x, arrayX[j]);
+            assertEquals(point.y, arrayY[j] * secondArrayY[j++]);
+        }
+        assertThrows(InconsistentFunctionsException.class, () ->
+                service1.subtract(factoryLLF.create(new double[]{1, 1.1, 1.2, 1.998, 1.999}, new double[]{6, 23, 511, 1000.01, 1000.2}),
+                        factoryATF.create(new double[]{1., 1.1, 1.2, 1.998}, new double[]{23, 511., 900., 1000.})));
     }
 
     @Test
     public void testDivider() {
+        LinkedListTabulatedFunctionFactory factoryLLF = new LinkedListTabulatedFunctionFactory();
+        TabulatedFunctionOperationService service1 = new TabulatedFunctionOperationService();
+        TabulatedFunctionOperationService service2 = new TabulatedFunctionOperationService(factoryLLF);
+        TabulatedFunction a = new ArrayTabulatedFunctionFactory().create(xValues, yValues);
+        TabulatedFunction b = factoryLLF.create(xValues, yValuesSecond);
+        TabulatedFunction dividerATF = service1.divider(a, b);
+        TabulatedFunction dividerLLF = service2.divider(a, b);
+        int i = 0;
+        for (Point point : dividerATF) {
+            assertEquals(point.x, xValues[i]);
+            assertEquals(point.y, yValues[i] / yValuesSecond[i++]);
+        }
+        int j = 0;
+        for (Point point : dividerLLF) {
+            assertEquals(point.x, xValues[j]);
+            assertEquals(point.y, yValues[j] / yValuesSecond[j++]);
+        }
+        assertThrows(InconsistentFunctionsException.class, () ->
+                service1.subtract(factoryLLF.create(new double[]{6., 7., 8.9, 10., 23}, new double[]{1.,10.,100.,1000.,10000.}), a));
     }
 }
