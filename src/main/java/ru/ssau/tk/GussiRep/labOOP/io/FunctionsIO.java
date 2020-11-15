c6aaca397f247f064d620abacf5703cee8b9d6c9
BeatsBoyXZP <bgv-gleb01@yandex.ru>
2020-11-15 21:38:41 +0300

    TabulatedFunctionFilereader

--- src/main/java/ru/ssau/tk/GussiRep/labOOP/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/GussiRep/labOOP/io/FunctionsIO.java
@@ -5,6 +5,9 @@ import ru.ssau.tk.GussiRep.labOOP.functions.TabulatedFunction;
 import ru.ssau.tk.GussiRep.labOOP.functions.factory.TabulatedFunctionFactory;
 
 import java.io.*;
+import java.util.Locale;
+import java.text.NumberFormat;
+import java.text.ParseException;
 
 public final class FunctionsIO {
     private FunctionsIO() {
@@ -20,7 +23,25 @@ public final class FunctionsIO {
        }
        printWriter.flush();
    }
+    public static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
+        int count = Integer.parseInt(reader.readLine());
 
+        double[] xValues = new double[count];
+        double[] yValues = new double[count];
+        NumberFormat numberFormat = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
+        for (int i = 0; i < count; i++) {
+            String line = reader.readLine();
+            String[] splitLine = line.split(" ");
+            try {
+                xValues[i] = numberFormat.parse(splitLine[0]).doubleValue();
+                yValues[i] = numberFormat.parse(splitLine[1]).doubleValue();
+            } catch (ParseException eParse) {
+                throw new IOException(eParse);
+            }
+        }
+
+        return factory.create(xValues, yValues);
+    }
 
     static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
         DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
