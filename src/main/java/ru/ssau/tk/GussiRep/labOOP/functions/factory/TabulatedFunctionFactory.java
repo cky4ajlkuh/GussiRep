d6a94508cdf474c90879c5fbb5d30383129617b5
Konstantinov <misha.tank.2014@mail.ru>
2020-11-19 21:56:09 +0400

    create createStrict + Test

--- src/main/java/ru/ssau/tk/GussiRep/labOOP/functions/factory/TabulatedFunctionFactory.java
+++ src/main/java/ru/ssau/tk/GussiRep/labOOP/functions/factory/TabulatedFunctionFactory.java
@@ -1,7 +1,13 @@
 package ru.ssau.tk.GussiRep.labOOP.functions.factory;
 
+import ru.ssau.tk.GussiRep.labOOP.functions.StrictTabulatedFunction;
 import ru.ssau.tk.GussiRep.labOOP.functions.TabulatedFunction;
 
 public interface TabulatedFunctionFactory {
+
+    default TabulatedFunction createStrict(double[] xValues, double[] yValues){
+        return new StrictTabulatedFunction(create(xValues,yValues));
+    };
+
     TabulatedFunction create(double[] xValues, double[] yValues);
 }
--- src/test/java/ru/ssau/tk/GussiRep/labOOP/functions/factory/TabulatedFunctionFactoryTest.java
+++ src/test/java/ru/ssau/tk/GussiRep/labOOP/functions/factory/TabulatedFunctionFactoryTest.java
@@ -3,6 +3,8 @@ package ru.ssau.tk.GussiRep.labOOP.functions.factory;
 import org.testng.annotations.Test;
 import ru.ssau.tk.GussiRep.labOOP.functions.ArrayTabulatedFunction;
 import ru.ssau.tk.GussiRep.labOOP.functions.LinkedListTabulatedFunction;
+import ru.ssau.tk.GussiRep.labOOP.functions.StrictTabulatedFunction;
+import ru.ssau.tk.GussiRep.labOOP.functions.TabulatedFunction;
 
 import static org.testng.Assert.*;
 
@@ -23,5 +25,24 @@ public class TabulatedFunctionFactoryTest {
         assertTrue(secondFunction.create(xValues2, yValues2) instanceof ArrayTabulatedFunction);
         assertTrue(firstFunction.create(xValues3, yValues3) instanceof LinkedListTabulatedFunction);
         assertTrue(secondFunction.create(xValues3, yValues3) instanceof ArrayTabulatedFunction);
+    }
+
+    @Test
+    public void testCreateStrict() {
+        double[] xValues = new double[]{1., 2., 3., 4., 5., 6., 7., 8., 9., 10.};
+        double[] yValues = new double[]{11., 12., 13., 14., 15., 16., 17., 18., 19., 20.};
+
+        TabulatedFunctionFactory tabulatedFunctionFactory = new LinkedListTabulatedFunctionFactory();
+        TabulatedFunction function = tabulatedFunctionFactory.create(xValues, yValues);
+        TabulatedFunction function1 = tabulatedFunctionFactory.createStrict(xValues, yValues);
+
+        TabulatedFunctionFactory tabulatedFunctionFactory1 = new ArrayTabulatedFunctionFactory();
+        TabulatedFunction function2 = tabulatedFunctionFactory1.create(xValues, yValues);
+        TabulatedFunction function3 = tabulatedFunctionFactory1.createStrict(xValues, yValues);
+        assertTrue(function instanceof LinkedListTabulatedFunction);
+        assertTrue(function1 instanceof StrictTabulatedFunction);
+        assertTrue(function2 instanceof ArrayTabulatedFunction);
+        assertTrue(function3 instanceof StrictTabulatedFunction);
+
     }
 }
