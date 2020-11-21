423dcd064810605720ecca3aaa0a16a033ee18ee
Konstantinov <misha.tank.2014@mail.ru>
2020-11-21 20:53:01 +0400

    create ReadWriteTaskExecutor (без реализации)

---
+++ src/main/java/ru/ssau/tk/GussiRep/labOOP/concurrent/ReadWriteTaskExecutor.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.GussiRep.labOOP.concurrent;
+
+import ru.ssau.tk.GussiRep.labOOP.functions.LinkedListTabulatedFunction;
+import ru.ssau.tk.GussiRep.labOOP.functions.TabulatedFunction;
+
+public class ReadWriteTaskExecutor {
+    public static void main(String[] args){
+    }
+}
