9309c32cf2f83c39520e347d70ea433c7d50291e
Konstantinov <misha.tank.2014@mail.ru>
2020-11-22 15:32:20 +0400

    realize ReadWriteTaskExecutor

--- src/main/java/ru/ssau/tk/GussiRep/labOOP/concurrent/ReadWriteTask.java
+++ src/main/java/ru/ssau/tk/GussiRep/labOOP/concurrent/ReadWriteTask.java
@@ -16,10 +16,11 @@ public class ReadWriteTask implements Runnable {
         for (int i = 0; i < tabulatedFunction.getCount(); i++) {
             x = tabulatedFunction.getX(i);
             y = tabulatedFunction.getY(i);
-            System.out.printf("%s, before write:  i = %d, x = %f, y = %f", Thread.currentThread().getName(), i, x, y);
+            System.out.printf(" %s, before write:  i = %d, x = %f, y = %f ", Thread.currentThread().getName(), i, x, y);
             tabulatedFunction.setY(i, y + 1);
             y = tabulatedFunction.getY(i);
-            System.out.printf("%s, after write: i = %d, x = %f, y = %f", Thread.currentThread().getName(), i, x, y);
+            System.out.printf(" %s, after write: i = %d, x = %f, y = %f ", Thread.currentThread().getName(), i, x, y);
+            System.out.println();
         }
     }
 }
--- src/main/java/ru/ssau/tk/GussiRep/labOOP/concurrent/ReadWriteTaskExecutor.java
+++ src/main/java/ru/ssau/tk/GussiRep/labOOP/concurrent/ReadWriteTaskExecutor.java
@@ -2,8 +2,23 @@ package ru.ssau.tk.GussiRep.labOOP.concurrent;
 
 import ru.ssau.tk.GussiRep.labOOP.functions.LinkedListTabulatedFunction;
 import ru.ssau.tk.GussiRep.labOOP.functions.TabulatedFunction;
+import ru.ssau.tk.GussiRep.labOOP.functions.ZeroFunction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReadWriteTaskExecutor {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
+        TabulatedFunction tabulatedFunction = new LinkedListTabulatedFunction(new ZeroFunction(), 1, 10, 10);
+        List<Thread> list = new ArrayList<>();
+        for (int i = 0; i <= 20; i++) {
+            Thread thread = new Thread(new ReadWriteTask(tabulatedFunction));
+            list.add(thread);
+        }
+        for(Thread thread: list){
+            thread.start();
+        }
+        Thread.sleep(3000);
+
     }
 }
