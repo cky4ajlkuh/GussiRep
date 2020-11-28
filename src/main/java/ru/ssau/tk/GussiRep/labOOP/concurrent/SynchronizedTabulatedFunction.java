aa552cca588c8a04860bc27b63eb8aef715ca603
Konstantinov <misha.tank.2014@mail.ru>
2020-11-28 13:06:22 +0400

    created SynchronizedTabulatedFunction + test

---
+++ src/main/java/ru/ssau/tk/GussiRep/labOOP/concurrent/SynchronizedTabulatedFunction.java
@@ -0,0 +1,84 @@
+package ru.ssau.tk.GussiRep.labOOP.concurrent;
+
+import ru.ssau.tk.GussiRep.labOOP.functions.Point;
+import ru.ssau.tk.GussiRep.labOOP.functions.TabulatedFunction;
+
+import java.util.Iterator;
+
+public class SynchronizedTabulatedFunction implements TabulatedFunction {
+    private final TabulatedFunction tabulatedFunction;
+
+    public SynchronizedTabulatedFunction(TabulatedFunction tabulatedFunction) {
+        this.tabulatedFunction = tabulatedFunction;
+    }
+
+    @Override
+    public int getCount() {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.getCount();
+        }
+    }
+
+    @Override
+    public double getX(int index) {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.getX(index);
+        }
+    }
+
+    @Override
+    public double getY(int index) {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.getY(index);
+        }
+    }
+
+    @Override
+    public void setY(int index, double value) {
+        synchronized (tabulatedFunction) {
+            tabulatedFunction.setY(index, value);
+        }
+    }
+
+    @Override
+    public int indexOfX(double x) {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.indexOfX(x);
+        }
+    }
+
+    @Override
+    public int indexOfY(double y) {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.indexOfY(y);
+        }
+    }
+
+    @Override
+    public double leftBound() {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.leftBound();
+        }
+    }
+
+    @Override
+    public double rightBound() {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.rightBound();
+        }
+    }
+
+    @Override
+    public Iterator<Point> iterator() {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.iterator();
+        }
+    }
+
+    @Override
+    public double apply(double x) {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.apply(x);
+        }
+    }
+}
---
+++ src/test/java/ru/ssau/tk/GussiRep/labOOP/concurrent/SynchronizedTabulatedFunctionTest.java
@@ -0,0 +1,109 @@
+package ru.ssau.tk.GussiRep.labOOP.concurrent;
+
+import org.testng.annotations.Test;
+import ru.ssau.tk.GussiRep.labOOP.functions.*;
+
+import java.util.Iterator;
+
+import static org.testng.Assert.*;
+
+public class SynchronizedTabulatedFunctionTest {
+
+    private final static double DELTA = 0.001;
+
+    private final static TabulatedFunction functionLLF = new LinkedListTabulatedFunction(new double[]{1., 2., 3., 4.}, new double[]{3., 6., 9., 12.});
+    private final static TabulatedFunction functionATF = new ArrayTabulatedFunction(new SqrFunction(), 1, 5, 5);
+    private final static SynchronizedTabulatedFunction synchronizedLLF = new SynchronizedTabulatedFunction(functionLLF);
+    private final static SynchronizedTabulatedFunction synchronizedATF = new SynchronizedTabulatedFunction(functionATF);
+
+
+    @Test
+    public void testGetCount() {
+        assertEquals(synchronizedLLF.getCount(),4, DELTA);
+        assertEquals(synchronizedATF.getCount(),5, DELTA);
+    }
+
+    @Test
+    public void testGetX() {
+        assertEquals(synchronizedLLF.getX(0),1, DELTA);
+        assertEquals(synchronizedLLF.getX(1),2, DELTA);
+
+        assertEquals(synchronizedATF.getX(0),1, DELTA);
+        assertEquals(synchronizedATF.getX(1),2, DELTA);
+    }
+
+    @Test
+    public void testGetY() {
+        assertEquals(synchronizedLLF.getY(0),3, DELTA);
+        assertEquals(synchronizedLLF.getY(1),6, DELTA);
+
+        assertEquals(synchronizedATF.getY(0),1, DELTA);
+        assertEquals(synchronizedATF.getY(1),4, DELTA);
+    }
+
+    @Test
+    public void testSetY() {
+        synchronizedATF.setY(0,10);
+        synchronizedLLF.setY(1,11);
+        assertEquals(synchronizedATF.getY(0),10, DELTA);
+        assertEquals(synchronizedLLF.getY(1),11, DELTA);
+        synchronizedATF.setY(0,12);
+        synchronizedLLF.setY(1,13);
+        assertEquals(synchronizedATF.getY(0),12, DELTA);
+        assertEquals(synchronizedLLF.getY(1),13, DELTA);
+
+    }
+
+    @Test
+    public void testIndexOfX() {
+        assertEquals(synchronizedLLF.indexOfX(2),1, DELTA);
+        assertEquals(synchronizedATF.indexOfX(1),0, DELTA);
+        assertEquals(synchronizedLLF.indexOfX(4),3, DELTA);
+    }
+
+    @Test
+    public void testIndexOfY() {
+        assertEquals(synchronizedLLF.indexOfY(3),0, DELTA);
+        assertEquals(synchronizedATF.indexOfY(25),4, DELTA);
+        assertEquals(synchronizedLLF.indexOfY(12),3, DELTA);
+    }
+
+    @Test
+    public void testLeftBound() {
+        assertEquals(synchronizedATF.leftBound(),1, DELTA);
+        assertEquals(synchronizedLLF.leftBound(),1, DELTA);
+    }
+
+    @Test
+    public void testRightBound() {
+        assertEquals(synchronizedATF.rightBound(),5, DELTA);
+        assertEquals(synchronizedLLF.rightBound(),4, DELTA);
+    }
+
+    @Test
+    public void testIterator() {
+        int i =0;
+        int j =0;
+        for(Point point : synchronizedLLF){
+            assertEquals(point.x, synchronizedLLF.getX(i++));
+            assertEquals(point.y, synchronizedLLF.getY(j++));
+        }
+        assertEquals(i, synchronizedLLF.getCount());
+        assertEquals(j, synchronizedLLF.getCount());
+         i =0;
+         j =0;
+        for(Point point : synchronizedATF){
+            assertEquals(point.x, synchronizedATF.getX(i++));
+            assertEquals(point.y, synchronizedATF.getY(j++));
+        }
+        assertEquals(i, synchronizedATF.getCount());
+        assertEquals(j, synchronizedATF.getCount());
+    }
+
+    @Test
+    public void testApply() {
+        assertEquals(synchronizedATF.apply(1),1, DELTA);
+        assertEquals(synchronizedLLF.apply(2),6, DELTA);
+        assertEquals(synchronizedLLF.apply(4),12, DELTA);
+    }
+}
