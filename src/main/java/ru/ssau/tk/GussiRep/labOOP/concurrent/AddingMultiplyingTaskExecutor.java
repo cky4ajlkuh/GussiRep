18c0141485ae4ca16f6deb6115e5091805d2d363
BeatsBoyXZP <bgv-gleb01@yandex.ru>
2020-12-08 17:55:20 +0400

    Сделан класс AddingMultiplyingTaskExecutor.

--- src/main/java/ru/ssau/tk/GussiRep/labOOP/concurrent/AddingMultiplyingTaskExecutor.java
+++ src/main/java/ru/ssau/tk/GussiRep/labOOP/concurrent/AddingMultiplyingTaskExecutor.java
@@ -1,4 +1,21 @@
 package ru.ssau.tk.GussiRep.labOOP.concurrent;
 
+import ru.ssau.tk.GussiRep.labOOP.functions.ZeroFunction;
+import ru.ssau.tk.GussiRep.labOOP.functions.ConstantFunction;
+import ru.ssau.tk.GussiRep.labOOP.functions.TabulatedFunction;
+import ru.ssau.tk.GussiRep.labOOP.functions.LinkedListTabulatedFunction;
+
 public class AddingMultiplyingTaskExecutor {
+    public static void main(String[] args) throws InterruptedException {
+        TabulatedFunction tabulatedFunction = new LinkedListTabulatedFunction(new ConstantFunction(2),1,100,100);
+        Thread thread1 = new Thread(new MultiplyingTask(tabulatedFunction));
+        Thread thread2 = new Thread(new MultiplyingTask(tabulatedFunction));
+        Thread thread3 = new Thread(new AddingTask(tabulatedFunction));
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        Thread.sleep(3000);
+        System.out.println(tabulatedFunction.toString());
+    }
 }
