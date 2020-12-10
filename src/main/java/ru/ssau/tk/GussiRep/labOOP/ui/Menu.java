b3216c6586e98f7e43cd735b2999d7d6b95df658
Konstantinov <misha.tank.2014@mail.ru>
2020-12-10 19:56:51 +0400

    добавил открывание окон при выборе всех остальных функций

--- src/main/java/ru/ssau/tk/GussiRep/labOOP/ui/Menu.java
+++ src/main/java/ru/ssau/tk/GussiRep/labOOP/ui/Menu.java
@@ -1,13 +1,18 @@
 package ru.ssau.tk.GussiRep.labOOP.ui;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
 
 public class Menu extends JFrame {
     JMenu menu = new JMenu("Functions");
     JMenuBar jMenuBar = new JMenuBar();
-    final createZeroFunction function = new createZeroFunction();
+    final CreateZeroFunction function = new CreateZeroFunction();
+    final CreatePowFunction function1 = new CreatePowFunction();
+    final CreateSqrFunction function2 = new CreateSqrFunction();
+    final CreateASinFunction function3 = new CreateASinFunction();
+    final CreateConstantFunction function4 = new CreateConstantFunction();
+    final CreateTabulatedFunction createTabulatedFunction = new CreateTabulatedFunction();
+
 
     Menu(String s) {
         super(s);
@@ -20,30 +25,18 @@ public class Menu extends JFrame {
 
     private JMenu createTabulatedFunction() {
         JMenu tabulatedFunction = new JMenu("TabulatedFunction");
-        tabulatedFunction.add(new JMenuItem("Ввести количество точек"));
+        JMenuItem tabFunction = new JMenuItem("Ввести количество точек");
+        tabFunction.addActionListener(event -> createTabulatedFunction.setVisible(true));
+        tabulatedFunction.add(tabFunction);
         return tabulatedFunction;
     }
 
-    private JMenu createMathFunction() {
-        JMenu mathFunction = new JMenu("MathFunction");
-        JMenuItem zero = new JMenuItem("Нулевая функция");
-        zero.addActionListener(event -> function.setVisible(true));
-        mathFunction.add(zero);
-        mathFunction.add(new JMenuItem("Квадратичная функция"));
-        mathFunction.add(new JMenuItem("Функция арксинуса"));
-        mathFunction.add(new JMenuItem("Функция корня"));
-        mathFunction.add(new JMenuItem("Тождественная функция"));
-
-        return mathFunction;
-    }
-
-
-    static class createZeroFunction extends JDialog {
-        JMenu menu = new JMenu("Нулевая функция");
+    static class CreateTabulatedFunction extends JDialog {
+        JMenu menu = new JMenu("Табулированная функция");
         JMenuBar jMenuBar = new JMenuBar();
 
-        public createZeroFunction() {
-            setSize(100, 100);
+        public CreateTabulatedFunction() {
+            setSize(400, 400);
             setVisible(false);
             setResizable(false);
             setLocationRelativeTo(null);
@@ -51,4 +44,99 @@ public class Menu extends JFrame {
             setJMenuBar(jMenuBar);
         }
     }
+
+    private JMenu createMathFunction() {
+        JMenu mathFunction = new JMenu("MathFunction");
+
+        JMenuItem zero = new JMenuItem("Нулевая функция");
+        zero.addActionListener(event -> function.setVisible(true));
+        mathFunction.add(zero);
+        JMenuItem aSin = new JMenuItem("Арксинус-функция");
+        aSin.addActionListener(event -> function3.setVisible(true));
+        mathFunction.add(aSin);
+        JMenuItem parabola = new JMenuItem("Квадратичная функция");
+        parabola.addActionListener(event -> function2.setVisible(true));
+        mathFunction.add(parabola);
+        JMenuItem pow = new JMenuItem("х в степени х функция");
+        pow.addActionListener(event -> function1.setVisible(true));
+        mathFunction.add(pow);
+        JMenuItem constant = new JMenuItem("Тождественная функция");
+        constant.addActionListener(event -> function4.setVisible(true));
+        mathFunction.add(constant);
+
+        return mathFunction;
+    }
+
+
+    static class CreateZeroFunction extends JDialog {
+        JMenu menu = new JMenu("Нулевая функция");
+        JMenuBar jMenuBar = new JMenuBar();
+
+        public CreateZeroFunction() {
+            setSize(400, 400);
+            setVisible(false);
+            setResizable(false);
+            setLocationRelativeTo(null);
+            jMenuBar.add(menu);
+            setJMenuBar(jMenuBar);
+        }
+    }
+
+    static class CreateASinFunction extends JDialog {
+        JMenu menu = new JMenu("Арксинус-функция");
+        JMenuBar jMenuBar = new JMenuBar();
+
+        public CreateASinFunction() {
+            setSize(400, 400);
+            setVisible(false);
+            setResizable(false);
+            setLocationRelativeTo(null);
+            jMenuBar.add(menu);
+            setJMenuBar(jMenuBar);
+        }
+    }
+
+    static class CreateSqrFunction extends JDialog {
+        JMenu menu = new JMenu("Парабола функция");
+        JMenuBar jMenuBar = new JMenuBar();
+
+        public CreateSqrFunction() {
+            setSize(400, 400);
+            setVisible(false);
+            setResizable(false);
+            setLocationRelativeTo(null);
+            jMenuBar.add(menu);
+            setJMenuBar(jMenuBar);
+        }
+    }
+
+    static class CreatePowFunction extends JDialog {
+        JMenu menu = new JMenu("х в степени х функция");
+        JMenuBar jMenuBar = new JMenuBar();
+
+        public CreatePowFunction() {
+            setSize(400, 400);
+            setVisible(false);
+            setResizable(false);
+            setLocationRelativeTo(null);
+            jMenuBar.add(menu);
+            setJMenuBar(jMenuBar);
+        }
+    }
+
+    static class CreateConstantFunction extends JDialog {
+        JMenu menu = new JMenu("х в степени х функция");
+        JMenuBar jMenuBar = new JMenuBar();
+
+        public CreateConstantFunction() {
+            setSize(400, 400);
+            setVisible(false);
+            setResizable(false);
+            setLocationRelativeTo(null);
+            jMenuBar.add(menu);
+            setJMenuBar(jMenuBar);
+        }
+    }
+
+
 }
