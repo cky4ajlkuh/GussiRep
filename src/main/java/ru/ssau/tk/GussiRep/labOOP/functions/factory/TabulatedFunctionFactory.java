5045327fa98c06c1e9a4ffb54de60bbc977c625b
BeatsBoyXZP <bgv-gleb01@yandex.ru>
2021-01-13 06:41:23 +0400

    Написал два метода для TabulatedFunctionFactory

--- src/main/java/ru/ssau/tk/GussiRep/labOOP/functions/factory/TabulatedFunctionFactory.java
+++ src/main/java/ru/ssau/tk/GussiRep/labOOP/functions/factory/TabulatedFunctionFactory.java
@@ -3,12 +3,21 @@ package ru.ssau.tk.GussiRep.labOOP.functions.factory;
 import ru.ssau.tk.GussiRep.labOOP.functions.MathFunction;
 import ru.ssau.tk.GussiRep.labOOP.functions.StrictTabulatedFunction;
 import ru.ssau.tk.GussiRep.labOOP.functions.TabulatedFunction;
+import ru.ssau.tk.GussiRep.labOOP.functions.UnmodifiableTabulatedFunction;
 
 public interface TabulatedFunctionFactory {
 
     default TabulatedFunction createStrict(double[] xValues, double[] yValues){
         return new StrictTabulatedFunction(create(xValues,yValues));
     };
+
+    default TabulatedFunction createStrictUnmodifiable(double[] xValues, double[] yValues) {
+        return new UnmodifiableTabulatedFunction(new StrictTabulatedFunction(create(xValues, yValues)));
+    }
+
+    default TabulatedFunction createUnmodifiable(double[] xValues, double[] yValues) {
+        return new UnmodifiableTabulatedFunction(create(xValues, yValues));
+    }
 
     TabulatedFunction create(double[] xValues, double[] yValues);
 
